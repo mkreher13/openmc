16aca7fd8b23c7b6b1cc5c4e215fb575f2faff36
Miriam <mkreher13@gmail.com>
2020-12-23 05:20:15 +0000

    Quick fix in nuclide.cpp

--- src/nuclide.cpp
+++ src/nuclide.cpp
@@ -396,7 +396,7 @@ void Nuclide::create_derived(const Function1D* prompt_photons, const Function1D*
           * xs_[t](i, XS_FISSION);
 	xs_[t](i, XS_PROMPT_NU_FISSION) = nu(E, EmissionMode::prompt)
           * xs_[t](i, XS_FISSION);
-	for (int d = 0; d <= n_precursor_; ++d) {
+	for (int d = 0; d < n_precursor_; ++d) {
 	  xs_[t](i, XS_DELAYED_NU_FISSION + d) = nu(E, EmissionMode::delayed, d+1)
 	    * xs_[t](i, XS_FISSION);
 	}
